c830760bab7db5f6cbd1cd515c9d2da65f72a3ba
Vlad <vecherkovsky2003@mail.ru>
2022-04-26 23:36:24 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Владичка Вечер on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
